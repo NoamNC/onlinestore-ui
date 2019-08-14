832ab07559d766fb6e0da982103e8d47eee0a694
Noam N <noamnc@gmail.com>
2019-08-14 20:47:27 +0300

    added formik and yup, created schemas and added validation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Register from './Register';
+
+class App extends React.Component{
+
+    render(){
+        return (
+            <div className ="dd">
+              <Register></Register>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Register.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import User from "./models/user";
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  send(values) {
+    fetch("http://localhost:4000/api/user", {
+      method: "PUT",
+      body: JSON.stringify(values),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+
+  render() {
+    return (
+        <div className="conatiner">
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          phone: ""
+        }}
+        validationSchema={User}
+        onSubmit={this.send.bind(this)}>
+        <Form>
+          <div className="form-grop">
+            <label>First name:</label>
+            <Field type="text" name="firstName" className="form-control" />
+            <ErrorMessage name="firstName" component="div" className="alert alert-danger"/>
+          </div>
+          <div className="form-grop">
+            <label>Last name:</label>
+            <Field type="text" name="lastName" className="form-control"/>
+            <ErrorMessage name="lastName" component="div" className="alert alert-danger"/>
+          </div>
+          <div className="form-grop">
+            <label>Email:</label>
+            <Field type="text" name="email" className="form-control"/>
+            <ErrorMessage name="email" component="div" className="alert alert-danger"/>
+          </div>
+          <div className="form-grop">
+            <label>Password:</label>
+            <Field type="password" name="password" className="form-control"/>
+            <ErrorMessage name="password" component="div" className="alert alert-danger"/>
+          </div>
+          <div className="form-grop">
+            <label></label>Phone:
+            <Field type="text" name="phone" className="form-control"/>
+            <ErrorMessage name="phone" component="div" className="alert alert-danger"/>
+          </div>
+          <div className="form-grop">
+            <input type="submit" value="submit" className="btn btn-primary" />
+          </div>
+        </Form>
+      </Formik>
+      </div>
+    );
+  }
+}
+export default Register;
